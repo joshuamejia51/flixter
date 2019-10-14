f40525ca1d7d1073fbed55ce240a1b784ab07438
Joshua Mejia <joshuamejia51@gmail.com>
2019-10-14 21:39:55 +0000

    lesson 14

--- app/controllers/insturctor/courses_controller.rb
+++ app/controllers/insturctor/courses_controller.rb
@@ -4,4 +4,24 @@ class Insturctor::CoursesController < ApplicationController
   def new
     @courses = course.new
   end
+
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
 end
+
+  def show 
+    @course = Course.find(params[:id])
+  end
+
+private
+
+def course_params
+  params.require(:course).permit(:title, :description, :cost)
+end
+
+end 
